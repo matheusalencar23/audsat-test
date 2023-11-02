84507a728703c2e136c262de0139f36b4e85634c
Matheus Alencar <matheusalencar6942@gmail.com>
2023-11-02 07:25:17 -0300

    header mobile

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
